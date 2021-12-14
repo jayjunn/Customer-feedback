76bc5c78092c70d2993e428fb2358ec3ace9321b
jayjunn <jayjunn@outlook.com>
2021-12-15 08:53:37 +0900

    change App to app

--- src/components/review_form/review_form.test.js
+++ src/components/review_form/review_form.test.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import ReviewForm from './review_form';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+
+const props = {
+  formValue: {
+    firstName: 'first',
+    lastName: 'last',
+    email: 'email',
+    rating: 5,
+    comment: 'comment',
+    timeStamp: 'string',
+  },
+  formIsHidden: true,
+  rating: 5,
+  submitted: true,
+  valid: true,
+  onclick: jest.MockedFunction(),
+  onChange: jest.fn,
+  onCancel: jest.fn,
+  onSubmit: jest.fn,
+  setRating: jest.fn,
+};
 
 it('renders inputs ', () => {
-  render(<ReviewForm />);
+  render(<ReviewForm {props} />);
   expect(screen.getByPlaceholderText('Enter your first name.'))
     .toBeInTheDocument;
   expect(screen.getByPlaceholderText('Enter your last name.'))
